b483409d9238f4ed835b957503a59ae2d02e0b21
Alem Bristric <alem.bristric@gmail.com>
2018-11-28 16:57:47 +0100

    First checkin  for ACM

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,45 @@
+﻿namespace ACM.BL
+{
+    public class Customer
+    {
+
+
+        public static int InstanceCount { get; set; }
+
+        private string _lastname;
+
+        public string Lastname
+        {
+            get { return _lastname; }
+            set { _lastname = value; }
+        }
+
+
+        public int CustomerID { get; private set; }
+        public string FirstName { get; set; }
+
+        public string EmailAdress { get; set; }
+
+
+
+
+        public string FullName
+        {
+            get
+            {
+                string fullName = Lastname;
+                if (!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if (!string.IsNullOrWhiteSpace(fullName))
+                    {
+                        fullName += ", ";
+                    }
+                    fullName += FirstName;
+                }
+                return fullName;
+            }
+        }
+
+
+    }
+}
---
+++ ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,81 @@
+﻿using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            //-- Arrange
+            Customer customer = new Customer();
+            customer.FirstName = "Bilbo";
+            customer.Lastname = "Baggins";
+
+            string expected = "Baggins, Bilbo";
+
+            //-- Act
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+            //-- Arrange
+            Customer customer = new Customer();
+            customer.Lastname = "Baggins";
+            string expected = "Baggins";
+
+            //-- Act
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            Customer customer = new Customer();
+            customer.FirstName = "Bilbo";
+            string expected = "Bilbo";
+
+            //--Act 
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+
+        [TestMethod]
+        public void StaticTest()
+        {
+            var c1 = new Customer();
+            c1.FirstName = "Bilbo";
+            Customer.InstanceCount += 1;
+
+            var c2 = new Customer();
+            c2.FirstName = "Frodo";
+            Customer.InstanceCount += 1;
+
+            var c3 = new Customer();
+            c3.FirstName = "Rosie";
+            Customer.InstanceCount += 1;
+
+            //--Act 
+
+            //--Assert 
+            Assert.AreEqual(3, Customer.InstanceCount);
+        }
+
+
+    }
+}
